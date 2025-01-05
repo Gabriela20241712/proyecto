64063cfa0c1303a244eb41b62bb8d9c1f296e4bb
Gabriela20241712 <esther.toala@casagrande.edu.ec>
2025-01-05 01:59:14 -0500

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -318,8 +318,8 @@ if 'view' not in st.session_state:
 
 if st.session_state['view'] == 'menu':
     if 'data' not in st.session_state:
-        #menu_opciones = ["Cargar Dataset Kaggle"]
-        #opcion = st.sidebar.selectbox("Seleccione una opción", menu_opciones)
+        menu_opciones = ["Cargar Dataset Kaggle"]
+        opcion = st.sidebar.selectbox("Seleccione una opción", menu_opciones)
 
         if opcion == "Cargar Dataset Kaggle":
             cargar_dataset_kaggle()
